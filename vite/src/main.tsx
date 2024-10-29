5d2180497a1f7b8acd8ead76234fd79cdfc341d4
tertle950 <tertle950@gmail.com>
2024-10-29 17:12:12 -0600

    Okay...This is how I'm doing Vite.

--- vite/src/main.tsx
+++ vite/src/main.tsx
@@ -1,7 +1,34 @@
 //import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import Container from 'react-bootstrap/Container';
+
+const Home: React.FC = () => {
+	return (
+		<Container className="p-3">
+			<p>
+				Welcome to <b>not-too-high.xyz</b>, the website for playing classic luck-pushing card games
+				that emphasize the running total.
+			</p>
+			<p>
+				We don't have much of anything right now...Check back soon!
+			</p>
+		</Container>
+	);
+};
 
 createRoot(document.getElementById('root')!).render(
-  <p>Still in progress :)</p>
-)
+	<div>
+		<header className="navbar bg-dark text-white site-header sticky-top py-2 justify-content-between">
+			<h1 className="container">Not Too High</h1>
+		</header>
+		<Home />
+		<footer className="footer fixed-bottom bg-dark text-white text-center">
+			<div>Not Too High designed by Thomas Ertle</div>
+			<div>(other copyright information will go here)</div>
+			<div>This is an open-source project. <a href="https://github.com/Tertle950/startup-for-class-2">Here's the Github!</a></div>
+		</footer>
+	</div>
+);

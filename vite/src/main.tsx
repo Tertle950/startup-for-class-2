f4bb3d77ba1e556f11ea5831edd467ca176e4793
tertle950 <tertle950@gmail.com>
2024-11-09 16:59:46 -0700

    I HATE REACT!!!!!

--- vite/src/main.tsx
+++ vite/src/main.tsx
@@ -1,21 +1,22 @@
 //import { StrictMode } from 'react'
 import React from 'react';
-//import ReactDOM from 'react-dom';
-import { createRoot } from 'react-dom/client';
+import ReactDOM from 'react-dom/client';
+//*
 import {
 	BrowserRouter,
-	NavLink,
+	Link,
+	Outlet,
 	Routes,
-	Navigate,
 	Route
-  } from 'react-router-dom'
+  } from 'react-router-dom';
+  // */
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-import Container from 'react-bootstrap/Container';
+import { Container, Button } from 'react-bootstrap';
 
 const Home: React.FC = () => {
 	return (
-		<Container className="p-3">
+		<Container>
 			<p>
 				Welcome to <b>not-too-high.xyz</b>, the website for playing classic luck-pushing card games
 				that emphasize the running total.
@@ -23,20 +24,51 @@ const Home: React.FC = () => {
 			<p>
 				We don't have much of anything right now...Check back soon!
 			</p>
+			<p>
+				<b><Link to="/join-host">Click here to start playing!</Link></b>
+			</p>
 		</Container>
 	);
 };
 
-createRoot(document.getElementById('root')!).render(
-	<div>
+//*
+const JoinHost: React.FC = () => {
+	return (
+		<Container>
+			<p>
+				Let's play a game! Want to join a game, or host one?
+			</p>
+			<Button>Join</Button> <Button>Host</Button>
+		</Container>
+	);
+}
+// */
+
+const MainPage: React.FC = () => {
+	return (
+	<BrowserRouter>
 		<header className="navbar bg-dark text-white site-header sticky-top py-2 justify-content-between">
-			<NavLink to="/"><h1 className="container">Not Too High</h1></NavLink>
+			<Link to="/"><h1 className="container">Not Too High</h1></Link>
 		</header>
-		<Route path="/" element={<Home />} exact />
+		<p>
+
+		</p>
+		{//*
+		}
+		
+			<Routes>
+				<Route path="/" element={<Home />} />
+				<Route path="/join-host" element={<JoinHost />} />
+			</Routes>
+		{// */
+		}
 		<footer className="footer fixed-bottom bg-dark text-white text-center">
 			<div>Not Too High designed by Thomas Ertle</div>
 			<div>(other copyright information will go here)</div>
 			<div>This is an open-source project. <a href="https://github.com/Tertle950/startup-for-class-2">Here's the Github!</a></div>
 		</footer>
-	</div>
-);
+	</BrowserRouter>
+	);
+}
+
+ReactDOM.createRoot(document.getElementById('root')!).render(<MainPage />)

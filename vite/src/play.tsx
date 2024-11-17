ae3e1fafe13233ad0d67a0cf81d46a69f5587adb
tertle950 <tertle950@gmail.com>
2024-11-16 20:33:07 -0700

    Play page progress, ft. images

--- vite/src/play.tsx
+++ vite/src/play.tsx
@@ -12,12 +12,14 @@ export const Play: React.FC = () => {
         <div>Doe A. Deer played a 5. The deck is now at 5.</div>
         <div>Jane Doe played an X, and chose a value of 10. The deck is now at 15.</div>
         <div>John Doe played a -2. The deck is now at 13.</div>
-
-        <div className="position-absolute bottom-0 start-50 translate-middle-y">
-            <img src="./assets/-1.png" />
-            <img src="./assets/-1.png" />
-            <img src="./assets/-1.png" />
-            <img src="./assets/-1.png" />
+        <div>
+            <img src="/cards/-2.png" />
+        </div>
+        <div>
+            <img src="/cards/9.png" />
+            <img src="/cards/5.png" />
+            <img src="/cards/1.png" />
+            <img src="/cards/-1.png" />
         </div>
         </Container>
     )

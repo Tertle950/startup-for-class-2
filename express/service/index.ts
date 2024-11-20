563744ff2de400b70ba22e8a46ad4e94785a6c0f
tertle950 <tertle950@gmail.com>
2024-11-20 13:38:33 -0700

    I LOVE OO PROGRAMMING sometimes

--- express/service/index.ts
+++ express/service/index.ts
@@ -9,6 +9,36 @@ app.get('/', (_req, res) => {
 
 const PORT = process.argv.length > 2 ? process.argv[2] : 3000;
 
+class User {
+	name: string;
+	email: string;
+	password: string;
+	token: string;
+
+	constructor(name: string, email: string, password: string, token: string) {
+		this.name = name;
+		this.email = email;
+		this.password = password; // TODO needs more security
+		this.token = token;
+	}
+}
+
+class GameState {
+	// TODO
+}
+
+class Game {
+	id: number;
+	game: string;
+	state: GameState;
+
+	constructor(id: number, game: string, state: GameState) {
+		this.id = id;
+		this.game = game;
+		this.state = state;
+	}
+}
+
 // JSON body parsing using built-in middleware
 app.use(express.json());
 
@@ -16,5 +46,5 @@ app.use(express.json());
 app.use(express.static('public'));
 
 app.listen(PORT, () => {
-  console.log(`Listening on port ${PORT}`);
+  console.log(`Now listening on port ${PORT}`);
 });
